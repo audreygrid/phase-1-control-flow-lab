17bf348f7022e06b833efd030eb966fc866635fb
Your GitHub Username <audreygride@gmail.com>
2025-03-14 10:56:27 +0300

    c

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  switch (true) {
+    case (tip === 'generous'):  // If the tip is generous (greater than 100)
+      return "Thank you so much.";
+    case (tip === "not as generous"):  // If the tip is not as generous (between 1 and 100)
+      return "Thank you.";
+    default:  // For any other cases (like zero or negative tip)
+      return "Bye.";
+  }
 }
+
